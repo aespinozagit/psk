80b4edae60d4304ec21cf9690ac46b46c9c98ce5
aespinozagit <aespinoza@gmail.com>
2016-05-05 17:10:55 -0500

    initial commit

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'q0rEkPLYTq8IkqwI3oy1l8soi');
+    define('CONSUMER_SECRET', '	VPMMeT77kVJla0mC9aOqhYIDcLzMJ8qj9mWOjWUSzsdx5TlVat');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '4902736754-PpI8YKuEcoIJapjeRJ4zj4yhGggllcXsAAW4TSS');
+    define('ACCESS_SECRET', 'LwQDYV250GAt5baKhTMsaRyQN0sTOJxCbgsMWVSihsHnj');
